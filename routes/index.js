602288b07bd9d070891f8b12d2045b693d884426
liyuhang <liyu@liyuhangdeMacBook-Pro.local>
2015-08-22 10:46:57 +0800

    代理,能爬10W用户了

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,18 @@
 var express = require('express');
 var router = express.Router();
 var superagent = require('superagent');
-var cheerio = require('cheerio');
+// var cheerio = require('cheerio');
+
 var mongoose = require('mongoose');
-// var crawl = require('./crawl.js');
+// var redis = require('redis');
 var config = require('./config.js');
 var db = mongoose.connect('mongodb://localhost/same');
+// var redisbs = redis.createClient(6379,'127.0.0.1');
+//设置代理
+require('superagent-proxy')(superagent);
+var proxys = ['http://115.239.210.166:80','http://202.121.32.63:80','http://111.13.12.163:80','http://121.40.71.207'];
+var proxy = process.env.http_proxy || 'http://115.239.210.166:80'
+
 // 创建视图
 var Schema = mongoose.Schema;
 var Users = new Schema({
@@ -24,24 +31,27 @@ var User = mongoose.model('user'),
 	Channel = mongoose.model('channel');
 
 var Crawl={
-	User_id : [{id:940}],
+	User_id : [{id: 1332290}],
 	User_hash : {},
-	Channel_id : [{id: 1399757}],
+	Channel_id : [{id: 1033563}],
 	Channel_hash : {},
 	host : "http://v2.same.com",
 	crawl_user : function(callback){
-		var self = this;console.log(self.Channel_id);
+		var self = this;
 		var obj = self.Channel_id.shift();
 		var url;var result;
 		if (!obj) {console.log("用户数量和用户心情已经被爬完..."); return ;}
 		if (obj.next){
 			url = self.host + obj.next;
 		}else{
-			url = self.host + '/user/' + obj.id + '/senses';
+			url = self.host + '/channel/' + obj.id + '/senses';
 		}
+		var proxy = proxys[Math.floor(Math.random()*4)]
 		superagent.get(url)
 				  .set(config.header)
+				  .proxy(proxy)
 				  .end(function(err,res){
+				  	//console.log('res: ',res);
 				  	if (err) {console.log(err);}
 				  	res.text = JSON.parse(res.text);
 				  	var data = res.text.data;
@@ -54,32 +64,83 @@ var Crawl={
 				  	}
 				  	//对数据库和队列操作
 				  	//爬完后存入hash
-				  	self.Channel_hash[obj.id] = true;
 				  	//与hash对比去重
 				  	var arr = [];var tmp;
-				  	for (var i = 0; i< data.results.length;i++){
+				  	for (var i = 0; i< data.results.length; i++) {
 				  		tmp = data.results[i].user.id
-				  		if( !self.User_hash[tmp]){
+				  		if (!self.User_hash[tmp]) {
+				  			 //console.log('UserID: ',tmp);
 				  			arr.push({id : tmp});
 				  			self.User_id.push({id : tmp});
+						  	self.User_hash[tmp] = true;
 				  		}
 				  	}
 				  	//数据库操作
 				  	User.create(arr,function(err,result){
 				  		if (err){ console.log(err)}
-				  		 // self.crawl_user();
+				  		 return self.crawl_user();
 				  	});
 				  });;
 
+	},
+	crawl_channel: function(callback){
+		var proxy = proxys[Math.floor(Math.random()*4)]
+		var self = this;
+		var obj = self.User_id.shift();
+		var url;var result;
+		if (!obj) {console.log("用户频道已经被爬完..."); return ;}
+		if (obj.next){
+			url = self.host + obj.next;
+		}else{
+			url = self.host + '/user/' + obj.id + '/channels/write';
+		}
+		superagent.get(url)
+				  .set(config.header)
+				  .proxy(proxy)
+				  .end(function(err,res){
+				  	if (err) {console.log(err);}
+				  	res.text = JSON.parse(res.text);
+				  	var data = res.text.data;
+				  	if (data.next){
+				  		//推入user_id队列中 下次继续爬
+				  		self.User_id.push({
+				  			id: obj.id,
+				  			next: data.next
+				  		});
+				  	}
+				  	//对数据库和队列操作
+				  	//爬完后存入hash
+				  	//与hash对比去重
+				  	var arr = [];var tmp;
+				  	for (var i = 0; i< data.writed_channels.length;i++){
+				  		tmp = data.writed_channels[i].channel.id
+				  		if( !self.Channel_hash[tmp]){
+				  			//console.log('ChannelId: ',tmp);
+				  			arr.push({id : tmp});
+				  			self.Channel_id.push({id : tmp});
+						  	self.Channel_hash[tmp] = true;
+				  		}
+				  	}
+				  	//数据库操作
+				  	Channel.create(arr,function(err,result){
+				  		if (err){ console.log(err)}
+				  		 return self.crawl_channel();
+				  	});
+				  });;		
+
 	}
+
 };
-/* GET home page. */
-router.get('/', function(req, res, next) {
+/* 爬user. */
+router.get('/user', function(req, res, next) {
 	console.log('开始爬取same...');
 	//从第一个userId开始爬channel
 	Crawl.crawl_user();
-  	console.log('运行完了');
 });
+//爬channel
+router.get('/channel',function(req,res,next){
+	Crawl.crawl_channel();
+})
 
 
 
